43bf88b90d52575042078b7da043b14888d1723d
kxxian <kristianryanbulos@gmail.com>
2026-01-16 08:51:50 +0800

    fixed typo in inngest index syncUserCreation function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -21,7 +21,7 @@ const syncUserCreation = inngest.createFunction(
       await prisma.user.update({
         where: { id: data.id },
         data: {
-          email: data?.email_addresses[0]?.email_addresses,
+          email: data?.email_addresses[0]?.email_address,
           name: data?.first_name + " " + data?.last_name,
           image: data?.image_url,
         },
@@ -31,7 +31,7 @@ const syncUserCreation = inngest.createFunction(
     await prisma.user.create({
       data: {
         id: data.id,
-        email: data?.email_addresses[0]?.email_addresses,
+        email: data?.email_addresses[0]?.email_address,
         name: data?.first_name + " " + data?.last_name,
         image: data?.image_url,
       },

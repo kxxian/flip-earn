6e8588a32085806c7c4a406ad7ff049b536f6d0e
kxxian <kristianryanbulos@gmail.com>
2026-01-30 11:31:48 +0800

    fixing authentication failed in nodemailer using Brevo

--- server/configs/nodemailer.js
+++ server/configs/nodemailer.js
@@ -3,8 +3,9 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
+  secure: false,
   auth: {
-    user: process.env.SMTP_USER,
+    user: process.env.SENDER_EMAIL,
     pass: process.env.SMTP_PASS,
   },
 });
